0cc2534aaf730723308cd158ea8d029a72eb312c
WesVpool <westonvanderpool@gmail.com>
2022-06-29 10:26:22 -0500

    first commit

---
+++ login.js
@@ -0,0 +1,42 @@
+function Login(){
+  const [show, setShow]         = React.useState(true);
+  const [status, setStatus]     = React.useState('');
+  const [name, setName]         = React.useState('');
+  const [email, setEmail]       = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const ctx = React.useContext(UserContext);  
+
+  function validate(field, label){
+    if (!field) {
+      setStatus('Error: ' + label);
+      setTimeout(() => setStatus(''),3000);
+      return false;
+    }
+    return true;
+}
+
+
+
+
+  return (
+    <Card
+    bgcolor="primary"
+    header="Account Login"
+    status={status}
+    body={show ? (  
+            <>
+            Email address<br/>
+            <input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
+            Password<br/>
+            <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)}/><br/>
+            <button type="submit" className="btn btn-light" onClick={loginUser}>Login</button>
+            </>
+          ):(
+            <>
+            <h5>Success</h5>
+            <button type="submit" className="btn btn-light" onClick={clearForm}>Login To Another Account</button>
+            </>
+          )}
+  />
+)
+}
