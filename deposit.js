75290534a5daa10e97910f4ad69a06a9d17d378d
WesVpool <westonvanderpool@gmail.com>
2022-06-29 14:40:03 -0500

    added login and deposit function

--- deposit.js
+++ deposit.js
@@ -1,5 +1,59 @@
 function Deposit(){
+  const ctx = React.useContext(UserContext);
+  const [show, setShow]         = React.useState(true);
+  const [status, setStatus]     = React.useState('');
+  const [name, setName]         = React.useState(ctx.users[0].name);
+  const [amount, setAmount]     = React.useState('');
+  const [balance, setBalance]   = React.useState(ctx.users[0].balance);
+
+  function validate(field) {
+    if (!Number(field)) {
+      alert("Input type not valid. Please enter a number");
+      setAmount('');
+      return false;
+    }
+    if (Number(field) <= 0) {
+      alert("Please enter a positive value");
+      setAmount('');
+      return false;
+    }
+    return true;
+  }
+
+  
+  function clearForm(){
+    setAmount('');
+    setShow(true);
+  }
+
+  function handleDeposit(){
+    if (!validate(amount, "amount")) return;
+    setBalance(ctx.users[0].balance += Number(amount));
+    setShow(false);
+  }
+  //console.log(ctx.users[0].name);
+  const persHeader = `${name}, Make A Deposit`
   return (
-    <h1>Deposit</h1>
+    <Card
+    bgcolor="primary"
+    header= {persHeader}
+    status={status}
+    body={show ? (  
+            <>
+            Current Balance: ${balance}<br/>
+            <br/>
+            Deposit Amount<br/>
+            <input type="input" className="form-control" id="amount" placeholder="Enter amount" value={amount} onChange={e => setAmount(e.currentTarget.value)}/><br/>
+            <button type="submit" className="btn btn-light" onClick={handleDeposit}>Deposit</button>
+            </>
+          ):(
+            <>
+            <h5>Success, {name}!</h5>
+            ${amount} has been deposited to your acount.<br/>
+            <h5>Your new account balance is: ${balance}</h5>
+            <button type="submit" className="btn btn-light" onClick={clearForm}>Make Another Deposit</button>
+            </>
+          )}
+  />
   )
 }
--- login.js
+++ login.js
@@ -15,8 +15,18 @@ function Login(){
     return true;
 }
 
-
-
+  function userLogin(){
+    console.log(email,password);
+    if (!validate(email,    'email'))    return;
+    if (!validate(password, 'password')) return;
+    const index1 = ctx.users.findIndex(x => x.email === `${email}`)
+    const log = ctx.users.splice(index1, 1)
+    ctx.users = [log[0], ...ctx.users]
+    console.log(ctx.users)
+    //ctx.users.push({name,email,password,balance:100});
+    setShow(false);
+  }
+  //const index = ctx.users.findIndex(x => x.email === `${email}`);
 
   return (
     <Card
@@ -29,12 +39,12 @@ function Login(){
             <input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
             Password<br/>
             <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)}/><br/>
-            <button type="submit" className="btn btn-light" onClick={loginUser}>Login</button>
+            <button type="submit" className="btn btn-light" onClick={userLogin}>Login</button>
             </>
           ):(
             <>
-            <h5>Success</h5>
-            <button type="submit" className="btn btn-light" onClick={clearForm}>Login To Another Account</button>
+            <h5>Success! Welcome {ctx.users[0].name}!</h5>
+            <button type="submit" className="btn btn-light" onClick={console.log("email")}>Login To Another Account</button>
             </>
           )}
   />
