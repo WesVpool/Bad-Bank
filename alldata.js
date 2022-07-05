439d92c991dc85deaef55bd498e6bcbda76b1d90
WesVpool <westonvanderpool@gmail.com>
2022-07-05 15:58:37 -0500

    transaction tracking to alldata

--- alldata.js
+++ alldata.js
@@ -1,27 +1,57 @@
 function AllData(){
   const ctx = React.useContext(UserContext);
+  console.log(ctx.users[0].balance);
 
-  function cardsUsers () {
-    return(
-      ctx.users.map((variations, index) => (
-        <Card
-          txtcolor="black"
-          header={ctx.users[index].name}
-          title="Welcome to United Bad Bank"
-          text="You can move around using the navigation bar."
-          body={<h5>All Data in Store {ctx.users[index].name}</h5>
-          }
-  />    
-      )));
+  function dollar (index) {
+    return `Current Balance: $${ctx.users[index].balance}`;
   }
+
+  function info (index) {
+    return(
+      <div>
+        <h5 className="fw-bold">Name: {ctx.users[index].name}</h5>
+        <h5 className="fw-bold">Email: {ctx.users[index].email}</h5>
+        <h5 className="fw-bold">Password: {ctx.users[index].password}</h5>
+      </div>
+    )
+  }
+
+  // function cardsUsers () {
+  //   return(
+  //     ctx.users.map((variations, index) => (
+  //       <Card
+  //         txtcolor="black"
+  //         header={ctx.users[index].name}
+  //         title={dollar(index)}s
+  //         body={<h5>All Data in Store {ctx.users[index].name}</h5>
+  //         }
+  // />    
+  //     )));
+  // }
 
   return (
     <>
     <h3>Users</h3>
     <br/>
-    {cardsUsers()}
+      {ctx.users.map((variations, index) => (
+        <Card
+          key="users"
+          txtcolor="black"
+          header={info(index)}
+          title={dollar(index)}
+          body={
+            <div>
+              <h5>{ctx.users[index].name}'s Transactions:</h5>
+
+              {ctx.users[index].trans.map((vary, i) => (
+                <div key={i}>
+                  <p>{ctx.users[index].trans[i]}</p>
+                </div>
+              ))}            
+
+            </div>
+          }/>    
+      ))}
     </>
   )
 }
-//<h5>All Data in Store</h5>
-//      {JSON.stringify(ctx)}<br/>
--- index.js
+++ index.js
@@ -2,14 +2,12 @@ function Spa() {
   return (
     <HashRouter>
       <NavBar/>
-      <UserContext.Provider value={{users:[{name:'Abel',email:'abel@mit.edu',password:'secret',balance:100}]}}>
+      <UserContext.Provider value={{users:[{name:'Abel',email:'abel@mit.edu',password:'secret',balance:100,trans:["Initial Account Balance $100"]}]}}>
         <div className="container" style={{padding: "20px"}}>
           <Route path="/" exact component={Home} />
           <Route path="/CreateAccount/" component={CreateAccount} />
-          {/* <Route path="/login/" component={Login} /> */}
           <Route path="/deposit/" component={Deposit} />
           <Route path="/withdraw/" component={Withdraw} />
-          {/* <Route path="/balance/" component={Balance} /> */}
           <Route path="/alldata/" component={AllData} />
         </div>
       </UserContext.Provider>      
@@ -23,4 +21,5 @@ ReactDOM.render(
 );
 
 
-// {name:'abel',email:'abel@mit.edu',password:'secret',balance:100}
+// /* <Route path="/balance/" component={Balance} /> */
+// /* <Route path="/login/" component={Login} /> */
