2580807b52f8536f7fcc505d107c5e0566216786
WesVpool <westonvanderpool@gmail.com>
2022-07-05 12:24:47 -0500

    added styling and editing alldata

--- alldata.js
+++ alldata.js
@@ -1,9 +1,27 @@
 function AllData(){
   const ctx = React.useContext(UserContext);
+
+  function cardsUsers () {
+    return(
+      ctx.users.map((variations, index) => (
+        <Card
+          txtcolor="black"
+          header={ctx.users[index].name}
+          title="Welcome to United Bad Bank"
+          text="You can move around using the navigation bar."
+          body={<h5>All Data in Store {ctx.users[index].name}</h5>
+          }
+  />    
+      )));
+  }
+
   return (
     <>
-    <h5>All Data in Store</h5>
-    {JSON.stringify(ctx)}<br/>
+    <h3>Users</h3>
+    <br/>
+    {cardsUsers()}
     </>
-  );
+  )
 }
+//<h5>All Data in Store</h5>
+//      {JSON.stringify(ctx)}<br/>
